92dc2aafdd1ecb83f7bd2534648d01d40f337da5
ZSabakh <zurabsabakhtarishvili@gmail.com>
2020-09-25 22:06:01 -0500

    Styling and delete handling

--- src/context/NewsContext.js
+++ src/context/NewsContext.js
@@ -3,7 +3,15 @@ import createContext from "./createContext";
 function newsReducer(state, action) {
   switch (action.type) {
     case "addNews":
-      return [...state, { title: `News #${state.length + 1}` }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: `News #${state.length + 1}`,
+        },
+      ];
+    case "deleteNews":
+      return state.filter((state) => state.id !== action.payload);
     default:
       return;
   }
@@ -15,8 +23,14 @@ function addNews(dispatch) {
   };
 }
 
+function deleteNews(dispatch) {
+  return (id) => {
+    dispatch({ type: "deleteNews", payload: id });
+  };
+}
+
 export const { Context, Provider } = createContext(
   newsReducer,
-  { addNews },
+  { addNews, deleteNews },
   []
 );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,25 +1,59 @@
 import React, { useContext } from "react";
-import { Text, StyleSheet, Button, FlatList, SafeAreaView } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  FlatList,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import { Context } from "../context/NewsContext";
+import { Foundation } from "@expo/vector-icons";
 
 function HomeScreen() {
-  const { state, addNews } = useContext(Context);
+  const { state, addNews, deleteNews } = useContext(Context);
 
   return (
     <SafeAreaView>
-      <Text>Hello</Text>
       <Button title="Add" onPress={() => addNews()} />
       <FlatList
         data={state}
         keyExtractor={(post) => post.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.postContainer}>
+              <Text style={styles.title}>{item.title}</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  deleteNews(item.id);
+                }}
+              >
+                <Foundation style={styles.icon} name="page-delete" />
+              </TouchableOpacity>
+            </View>
+          );
         }}
       />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  postContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+    borderBottomWidth: 2,
+    paddingHorizontal: 15,
+    borderColor: "navy",
+  },
+  title: {
+    fontSize: 20,
+  },
+  icon: {
+    fontSize: 28,
+  },
+});
 
 export default HomeScreen;
